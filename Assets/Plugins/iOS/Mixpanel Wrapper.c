316cd2f237e3185649a7fce3ed31e5921b365ab5
System Administrator <root@Ryan-Jungs-MacBook-Pro.local>
2015-09-21 14:51:23 -0700

    fixed char * (no string in C)

--- Assets/Plugins/iOS/Mixpanel Wrapper.c
+++ Assets/Plugins/iOS/Mixpanel Wrapper.c
@@ -1,7 +1,7 @@
-﻿void mixpanel_init(string *token) {
+﻿void mixpanel_init(char *token) {
 	[mixpanel sharedInstanceWithToken:token];
 }
 
-void mixpanel_track(string *event, string *property) {
+void mixpanel_track(char *event, char *property) {
 	[mixpanel track:event properties:property];
 }
