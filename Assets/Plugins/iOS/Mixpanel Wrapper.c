dbc36bd0c9e28c1cf5ae02173fb79309f53714d6
System Administrator <root@Ryan-Jungs-MacBook-Pro.local>
2015-09-21 11:04:26 -0700

    Mixpanel Wrapper

---
+++ Assets/Plugins/iOS/Mixpanel Wrapper.c
@@ -0,0 +1,7 @@
+﻿void mixpanel_init(string *token) {
+	[mixpanel sharedInstanceWithToken:token];
+}
+
+void mixpanel_track(string *event, string *property) {
+	[mixpanel track:event properties:property];
+}
