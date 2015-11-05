ab99f5afca4e295fb91f8c0b8f044be7bdc5d306
Ryan Jung <ryan_jung@mba.berkeley.edu>
2015-11-05 14:12:50 -0800

    Android code added

---
+++ Desktop/Unity-Game---Roll-a-Ball-mixpanel/Assets/Wrappers/MP_Unity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MP_Unity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MySDK.MixpanelInit ("wordpressapp");
+		MySDK.MixpanelTrack ("Game Started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
